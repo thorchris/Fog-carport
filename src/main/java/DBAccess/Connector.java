ea38a535cdce32fdf4ad485779356956d2ce804b
thorchris <56426781+thorchris@users.noreply.github.com>
2020-04-23 08:48:31 +0200

    connector update

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -11,22 +11,39 @@ import java.sql.SQLException;
  */
 public class Connector {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/fogcarport?serverTimezone=CET";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "green8house17";
+    private static String URL;
+    private static String USERNAME;
+    private static String PASSWORD;
 
     private static Connection singleton;
 
-    public static void setConnection( Connection con ) {
+    public static void setConnection(Connection con) {
         singleton = con;
     }
 
     public static Connection connection() throws ClassNotFoundException, SQLException {
-        if ( singleton == null ) {
-            Class.forName( "com.mysql.cj.jdbc.Driver" );
-            singleton = DriverManager.getConnection( URL, USERNAME, PASSWORD );
+        setDBCCredentialts();
+        if (singleton == null || singleton.isClosed()) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            singleton = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         }
         return singleton;
     }
 
+    public static void setDBCCredentialts() {
+        String deplyoed = System.getenv("DEPLOYED");
+
+        if (deplyoed != null) {
+            //SERVER
+            URL = System.getenv("JDBC_CONNECTION_STRING");
+            USERNAME = System.getenv("JDBC_USER");
+            PASSWORD = System.getenv("JDBC_PASSWORD");
+        } else {
+            //LOCALHOST
+            URL = "jdbc:mysql://localhost:3306/fogcarport?serverTimezone=CET&useSSL=false";
+            USERNAME = "root";
+            PASSWORD = "green8house17";
+        }
+    }
 }
+
