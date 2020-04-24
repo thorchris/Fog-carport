2aea58f1e8747a211d6001585e2b36abff267070
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2020-04-24 11:31:19 +0200

    Added CarportMat, updated Datamapper

---
+++ src/main/java/FunctionLayer/CarportMaterials.java
@@ -0,0 +1,41 @@
+package FunctionLayer;
+
+public class CarportMaterials {
+    private String materialName;
+    private int materialID;
+    private double materialPriceM;
+    private double width;
+    private double length;
+
+    public CarportMaterials(String materialName, int materialID, double materialPriceM, double width, double length) {
+        this.materialName = materialName;
+        this.materialID = materialID;
+        this.materialPriceM = materialPriceM;
+        this.width = width;
+        this.length = length;
+    }
+
+    public String getMaterialName() {
+        return materialName;
+    }
+
+    public void setMaterialName(String materialName) {
+        this.materialName = materialName;
+    }
+
+    public int getMaterialID() {
+        return materialID;
+    }
+
+    public void setMaterialID(int materialID) {
+        this.materialID = materialID;
+    }
+
+    public double getMaterialPriceM() {
+        return materialPriceM;
+    }
+
+    public void setMaterialPriceM(double materialPriceM) {
+        this.materialPriceM = materialPriceM;
+    }
+}
