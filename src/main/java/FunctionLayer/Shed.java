2572698e6f59eb84e88b7adddacd086345cbebbe
thorchris <56426781+thorchris@users.noreply.github.com>
2020-05-03 17:03:00 +0200

    Shed test

--- src/main/java/FunctionLayer/Shed.java
+++ src/main/java/FunctionLayer/Shed.java
@@ -4,20 +4,29 @@ import Util.CalcShedMaterials;
 
 public class Shed {
     /**
-     * @globalvar: carportLength
-     * @globalvar: carportWidth
-     * @globalvar: shedWidth
-     * @globalvar: shedLength
-     * @globalvar: totalShedCladding
-     * @globalvar: straps
-     * @globalvar: totalShedCladding
-     * @globalvar: totalShedCladding
+     * @globalvar: carportLength - The carport length is used to determine the length of the shed you can choose.
+     * @globalvar: carportWidth - The carport width is used to determine the width of the shed
+     * @globalvar: shedWidth - Shed width is determined from the width of the carport (half or full)
+     * @globalvar: shedLength - Shed length is set values of either 1,2 or 3 meters
+     * @globalvar: totalShedCladding - Total shed cladding is a variable holding the total amount of cladding needed for the shed
+     * @globalvar: straps - Straps is dertermined by width of the carport
+     * @globalvar: doorKnob - One doorknob is always used
+     * @globalvar: doorHinges - Two doorhinges always needed
+     * @globalvar: shedMaterials - Shed Materials is the materials you can choose to have your shed built from
      */
 
     private double carportLength, carportWidth, shedWidth, shedLength, totalShedCladding, straps;
     private boolean isHalfWidth;
     private int doorKnob, doorHinges;
     private ShedMaterials shedMaterials;
+
+    /**
+     *
+     * @param carportLength The carport length is used to determine the length of the shed you can choose.
+     * @param carportWidth  The carport width is used to determine the width of the shed
+     * @param isHalfWidth This boolean is used for two options to chose size of the shed - half the width of the carport or the whole width of the carport
+     * @param shedMaterials ShedMaterials is a class containing all materials used to build out a shed, doorknobs, doorhinges etc..
+     */
 
     public Shed(double carportLength, double carportWidth,boolean isHalfWidth, ShedMaterials shedMaterials) {
         CalcShedMaterials calcShedMaterials = new CalcShedMaterials(this);
@@ -28,16 +37,9 @@ public class Shed {
         this.isHalfWidth = isHalfWidth;
         this.shedMaterials = shedMaterials;
         setShedLength();
+        setShedWidth();
         this.totalShedCladding = calcShedMaterials.calculateShedCladding();
-        this.straps = calcShedMaterials.calculateStraps();
-
-        //If the shed is half width make it half of carport width, otherwise full width.
-        if(isHalfWidth){
-            shedWidth = carportWidth/2;
-        } else{
-            shedWidth = carportWidth;
-        }
-
+        this.straps = calcShedMaterials.calculateStrapsLength();
     }
 
      public double getCarportLength() {
@@ -75,10 +77,19 @@ public class Shed {
     public void setShedLength() {
         if (carportLength <= 3.6) {
             shedLength = 1.0;
-        } else if (carportLength <= 5.1) {
+        }else if (carportLength <= 5.1 && carportLength > 3.6) {
             shedLength = 2.0;
-        } else if (carportLength <= 7.8) {
+        }else if (carportLength <= 7.8 && carportLength > 5.1) {
             shedLength = 3.0;
+        }
+    }
+
+    public void setShedWidth(){
+        //If the shed is half width make it half of carport width, otherwise full width.
+        if(isHalfWidth){
+            shedWidth = carportWidth/2;
+        } else{
+            shedWidth = carportWidth;
         }
     }
 
