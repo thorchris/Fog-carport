b8a8ffa90af7b8c0b339d74c164dc0f31b6a6c92
hallursimonsen <hallur3395@gmail.com>
2020-05-04 11:55:08 +0200

    Test updated with straps n stuff

--- src/main/java/Util/CalculateCarportPartsPrice.java
+++ src/main/java/Util/CalculateCarportPartsPrice.java
@@ -10,6 +10,17 @@ public class CalculateCarportPartsPrice {
         this.carportParts = carportParts;
     }
 
+    /**
+     *
+     * @param screwPrice is the price for each screw
+     * @param rafterPrice is the price for every meter of rafter
+     * @param postPrice is the price per post
+     * @param strapPrice is the price for every meter of strap
+     * @param carportMaterials 
+     * @return the total price for all the materials needed for a carport
+     * the method get's it's values from the class carportParts, which we get through the constructor
+     */
+
     public double calculateCarportPartPrice(double screwPrice, double rafterPrice, double postPrice, double strapPrice, CarportMaterials carportMaterials){
         double totalScrewPrice = carportParts.getTotalScrews() * screwPrice;
         double totalRafterPrice = carportParts.getTotalRafters() * rafterPrice;
