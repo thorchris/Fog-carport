64ca7e254c6a0cdc6a5989bd3275f1527d25072c
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2020-05-05 12:11:02 +0200

    index fix måske

--- src/main/java/FunctionLayer/LogicFacade.java
+++ src/main/java/FunctionLayer/LogicFacade.java
@@ -36,34 +36,4 @@ public class LogicFacade {
     public static User login(String email, String password) throws LoginSampleException {
         return UserMapper.login(email, password);
     }
-
-    public static void main(String[] args) {
-        UserMapper um = new UserMapper();
-
-        User user = new User("Josef@ja.dk", "kode");
-        user.setId(12);
-        User user2 = new User("sefef@ja.dk", "kode");
-
-
-        um.getUserId("josef@tis.dk");
-        System.out.println(um.getUserId("josef@tis.dk"));
-        double length = 2;
-        double width = 2;
-        boolean hasAShed = true;
-        boolean isHalfWidth = true;
-        CarportMaterials carportMaterials = new CarportMaterials("testMateriale", 1, 14, 0.15, 3);
-        int sidesWithCladding = 2;
-        CarportParts carportParts = new CarportParts(length, width, hasAShed, isHalfWidth, carportMaterials, sidesWithCladding);
-
-        ShedMaterials shedMaterials = new ShedMaterials();
-        Shed shed = new Shed(length, width, isHalfWidth, shedMaterials);
-
-        boolean isHighRoof = true;
-        RoofMaterials roofMaterials = new RoofMaterials("testMateriale", 1, 25, 10,15);
-        //RoofMaterials roofmaterial,double carportLength, double carportWidth
-        Roof roof = new Roof(isHighRoof, roofMaterials,length, width);
-        FullCarport carport = new FullCarport(carportParts, roof, shed);
-
-        LogicFacade.insertData(user, carport);
-    }
 }
