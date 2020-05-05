122175f34644f91b0a345226b6a41813eb834bd0
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2020-05-05 10:29:51 +0200

    Added login, register

---
+++ src/main/java/PresentationLayer/Login.java
@@ -0,0 +1,45 @@
+package PresentationLayer;
+
+import FunctionLayer.LogicFacade;
+import FunctionLayer.LoginSampleException;
+import FunctionLayer.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Her er en klasse der gør at kunden kan logge ind, hvis det er en admin der logger ind viderestilles administratoren til admin.jsp.
+ *
+ * @author Thor, Hallur, Josef og Frederik
+ */
+public class Login extends Command {
+    /**
+     * @param request - den aktuelle HTTP request
+     * @param response - den aktuelle HTTP servlet response
+     * @return - en string, som er den side der skal returneres når metoden er kort.
+     * Denne sendes til frontcontrolleren, hvor der tilsættes den rette sti (WEB-INF/ ... )
+     * @throws - LoginSampleException, en hjemmelavet exception som propageres (VI KASTER DEN BARE VÆK) og ikke håndteres.
+     * @version - Færdige version for cupcake projektet.
+     */
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        User user = LogicFacade.login(email, password);
+
+        HttpSession session = request.getSession();
+
+        session.setAttribute("email", email);  // ellers skal man skrive  user.email på jsp siderne og det er sgu lidt mærkeligt at man har adgang til private felter. Men måske er det meget fedt , jeg ved det ikke
+        session.getServletContext().setAttribute("user", user);
+
+        switch (email) {
+            case "sælger":
+                return "sælger";
+            default:
+                return "../index";
+        }
+
+    }
+
+}
