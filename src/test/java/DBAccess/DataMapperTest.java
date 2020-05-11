eadee9814bec2d6b5ce9c440daa8f80066310f47
hallursimonsen <hallur3395@gmail.com>
2020-05-11 09:52:39 +0200

    datamapper test

--- src/test/java/DBAccess/DataMapperTest.java
+++ src/test/java/DBAccess/DataMapperTest.java
@@ -7,12 +7,12 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import FunctionLayer.CarportMaterials;
-import FunctionLayer.FullCarport;
-import FunctionLayer.Order;
+import FunctionLayer.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 
 import javax.xml.crypto.Data;
@@ -22,7 +22,7 @@ public class DataMapperTest {
 
     private static Connection testConnection;
     private static String USER = "root";
-    private static String USERPW = "kode";
+    private static String USERPW = "H7114bhs";
     private static String DBNAME = "fogcarport_test?serverTimezone=CET&useSSL=false";
     private static String HOST = "localhost";
 
@@ -30,47 +30,58 @@ public class DataMapperTest {
     public static void setUpConnection() {
         try {
             // awoid making a new connection for each test
-            if ( testConnection == null ) {
-                String url = String.format( "jdbc:mysql://%s:3306/%s", HOST, DBNAME );
-                Class.forName( "com.mysql.cj.jdbc.Driver" );
+            if (testConnection == null) {
+                String url = String.format("jdbc:mysql://%s:3306/%s", HOST, DBNAME);
+                Class.forName("com.mysql.cj.jdbc.Driver");
 
-                testConnection = DriverManager.getConnection( url, USER, USERPW );
+                testConnection = DriverManager.getConnection(url, USER, USERPW);
                 // Make mappers use test
-                Connector.setConnection( testConnection );
+                Connector.setConnection(testConnection);
             }
             // reset test database
-            try ( Statement stmt = testConnection.createStatement() ) {
-                stmt.execute( "DROP TABLE if EXISTS carport_materials" );
-                stmt.execute( "CREATE TABLE carport_materials LIKE fogcarport.carport_materials" );
-                stmt.execute( "INSERT INTO carport_materials SELECT * FROM fogcarport.carport_materials" );
+            try (Statement stmt = testConnection.createStatement()) {
+                stmt.execute("DROP TABLE if EXISTS carport_materials");
+                stmt.execute("CREATE TABLE carport_materials LIKE fogcarport.carport_materials");
+                stmt.execute("INSERT INTO carport_materials SELECT * FROM fogcarport.carport_materials");
 
-                stmt.execute( "DROP TABLE if EXISTS orders" );
-                stmt.execute( "CREATE TABLE orders LIKE fogcarport.order" );
-                stmt.execute( "INSERT INTO orders SELECT * FROM fogcarport.order" );
+                stmt.execute("DROP TABLE if EXISTS roof_materials");
+                stmt.execute("CREATE TABLE roof_materials LIKE fogcarport.roof_materials");
+                stmt.execute("INSERT INTO roof_materials SELECT * FROM fogcarport.roof_materials");
+
+                stmt.execute("DROP TABLE if EXISTS products");
+                stmt.execute("CREATE TABLE products LIKE fogcarport.products");
+                stmt.execute("INSERT INTO products SELECT * FROM fogcarport.products");
+
+                stmt.execute("DROP TABLE if EXISTS orders");
+                stmt.execute("CREATE TABLE orders LIKE fogcarport.order");
+                stmt.execute("INSERT INTO orders SELECT * FROM fogcarport.order");
+
+                stmt.execute( "DROP TABLE if EXISTS users" );
+                stmt.execute( "CREATE TABLE users LIKE fogcarport.users" );
+                stmt.execute( "INSERT INTO users SELECT * FROM fogcarport.users" );
             }
 
-        } catch ( ClassNotFoundException | SQLException ex ) {
+        } catch (ClassNotFoundException | SQLException ex) {
             testConnection = null;
-            System.out.println( "Could not open connection to database: " + ex.getMessage() );
+            System.out.println("Could not open connection to database: " + ex.getMessage());
         }
-
 
 
     }
 
 
-     //1	Egetræsbrædder	14	0.15	3
-     //2	Bøgetræsplade	12	0.15	3
-     //3	Plastiktræ	10	0.15	3
+    //1	Egetræsbrædder	14	0.15	3
+    //2	Bøgetræsplade	12	0.15	3
+    //3	Plastiktræ	10	0.15	3
 
     @Test
     public void testSetUpOK() {
         // Just check that we have a connection.
-        assertNotNull( testConnection );
+        assertNotNull(testConnection);
     }
 
     @Test
-    public void testGetCarportMaterialListSize(){
+    public void testGetCarportMaterialListSize() {
         List<CarportMaterials> carportMatListDB;
         carportMatListDB = DataMapper.getCarportMaterialsList();
 
@@ -80,12 +91,12 @@ public class DataMapperTest {
     }
 
     @Test
-    public void testGetCarportMaterialList(){
+    public void testGetCarportMaterialList() {
         List<CarportMaterials> carportMatList = new ArrayList<>();
 
-        CarportMaterials cm1 = new CarportMaterials("Egetræsbrædder" , 1, 14,0.15,3);
-        CarportMaterials cm2 = new CarportMaterials("Bøgetræsplade" , 2, 12,0.15,3);
-        CarportMaterials cm3 = new CarportMaterials("Plastiktræ" , 3, 10,0.15,3);
+        CarportMaterials cm1 = new CarportMaterials("Egetræsbrædder", 1, 14, 0.15, 3);
+        CarportMaterials cm2 = new CarportMaterials("Bøgetræsplade", 2, 12, 0.15, 3);
+        CarportMaterials cm3 = new CarportMaterials("Plastiktræ", 3, 10, 0.15, 3);
 
         carportMatList.add(cm1);
         carportMatList.add(cm2);
@@ -101,13 +112,38 @@ public class DataMapperTest {
     }
 
     @Test
-    public void testAddOrder(){
-        Order order = new Order(3, 4,2,4,5,6,7,8,9,1,2);
+    public void testAddOrder()throws LoginSampleException {
+
+
+        //Skaber alt data til at lave en fuld carport
+        CarportMaterials carportMaterials = new CarportMaterials("Bøgetræsplade", 2, 12, 0.15, 3);
+        CarportParts carportParts = new CarportParts(510, 330, true, false, carportMaterials, 1);
+
+        RoofMaterials roofmaterials = new RoofMaterials("Betontagsten - rød", 2, 450, 1, 2);
+        Roof roof = new Roof(false, roofmaterials, 510, 330);
+
+        ShedMaterials shedMaterials = new ShedMaterials();
+        Shed shed = new Shed(510, 330, false, shedMaterials);
+
+        //instantierer en ny carport med alt dataet ovenfor
+        FullCarport carport = new FullCarport(carportParts, roof, shed);
+        User original = new User( "josef", "hallur");
+        original.setId(2);
+        UserMapper.createUser( original );
+
+        int rafters = (int) carport.getCarportParts().getTotalRafters();
+        int cladding = (int) carport.getCarportParts().getCarportCladding();
+        int screws = (int) ((int) carport.getCarportParts().getTotalScrews() + carport.getRoof().getScrew()); ;
+        int posts = carport.getCarportParts().getTotalPosts();
+        int fascia = (int) carport.getRoof().getFascia();
+        int brackets = (int) carport.getRoof().getBracket();
+        int straps = carport.getCarportParts().getAmountOfStraps();
+        int doorKnobs = carport.getShed().getDoorKnob();
+        int doorHinges = carport.getShed().getDoorHinges();
+        int user_Id = original.getId();
+
+        DataMapper.addOrder(original, carport);
+
+
     }
-
-
-
-
-
-
 }
