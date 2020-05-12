fb035515d1a0b967d28f8793be2ee33685750e36
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-05-12 19:03:00 +0200

    Shed update
    Not quite there yet....

--- src/main/java/PresentationLayer/Drawing.java
+++ src/main/java/PresentationLayer/Drawing.java
@@ -13,34 +13,36 @@ public class Drawing extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException, OrderException {
         HttpSession session = request.getSession();
-        FullCarport fullCarport= null;
-        if(fullCarport == null){
+        FullCarport fullCarport = null;
+        if (fullCarport == null) {
             new CalculatePrice().execute(request, response);
             fullCarport = (FullCarport) session.getAttribute("fullCarport");
         }
 
-        double carportHeight = fullCarport.getCarportParts().getLength();
-        double carportWidth = fullCarport.getCarportParts().getWidth();
+        double carportWidth = fullCarport.getCarportParts().getLength();
+        double carportHeight = fullCarport.getCarportParts().getWidth();
         //Omregnes fordi de er m på siden.
-        int intCarportHeight = (int) (carportHeight*100);
-        int intCarportWidth = (int) (carportWidth*100);
+        int intCarportHeight = (int) (carportHeight * 100);
+        int intCarportWidth = (int) (carportWidth * 100);
 
-        Svg svg = new Svg(780, 750, "0,0,800,600",0,0);
+        Svg svg = new Svg(780, 750, "0,0,800,600", 0, 0);
 
         //Carport
-        svg.addRect(0,0,intCarportHeight,intCarportWidth);
+        svg.addRect(0, 0, intCarportHeight, intCarportWidth);
         //svg.addRect(0,0,600,780);
 
         //Posts
-        svg.addPosts(svg, fullCarport, intCarportWidth, intCarportHeight);
+        svg.addPosts(svg, fullCarport, intCarportHeight, intCarportWidth);
 
-        if(fullCarport.getCarportParts().isHasAShed()) {
+        if (fullCarport.getCarportParts().isHasAShed()) {
             double shedWidth = fullCarport.getShed().getShedWidth();
             double shedLength = fullCarport.getShed().getShedLength();
             int intShedWidth = (int) (shedWidth * 100);
-            int intShedLength = (int) (shedLength * 100);
+            int intShedHeight = (int) (shedLength * 100);
             //Shed
-            svg.addRect(-intCarportWidth,intShedWidth,-intShedLength,intCarportHeight);
+
+            svg.addRect(intCarportWidth, 0, intShedHeight, intShedWidth);
+
         }
         //int x, int y, int height, int width
 
