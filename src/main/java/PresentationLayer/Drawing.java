457f4539ace49f2ec3c4068b0d75019f07afe981
Frederik Dahl <fdah@itu.dk>
2020-05-12 13:32:57 +0200

    First draft for posts in SVG

--- src/main/java/PresentationLayer/Drawing.java
+++ src/main/java/PresentationLayer/Drawing.java
@@ -1,27 +1,47 @@
 package PresentationLayer;
 
+import FunctionLayer.FullCarport;
 import FunctionLayer.LoginSampleException;
 import FunctionLayer.OrderException;
 import FunctionLayer.Svg;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Drawing extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException, OrderException {
-        double height = Double.parseDouble(request.getParameter("width"));
-        double width = Double.parseDouble(request.getParameter("length"));
+        HttpSession session = request.getSession();
+        FullCarport fullCarport= null;
+        if(fullCarport == null){
+            new CalculatePrice().execute(request, response);
+            fullCarport = (FullCarport) session.getAttribute("fullCarport");
+        }
+
+        double height = fullCarport.getCarportParts().getLength();
+        double width = fullCarport.getCarportParts().getWidth();
+        //Omregnes fordi de er m på siden.
         int intHeight = (int) (height*100);
         int intWidth = (int) (width*100);
 
-        Svg svg = new Svg(800, 600, "0,0,800,600",0,0);
-        Svg svgInnerDrawing = new Svg(900,800,"0,0,900,800",0,0);
 
-        //Basis carport
-        svg.addRect(0,0,600,780);
+        Svg svg = new Svg(900, 800, "0,0,800,600",0,0);
 
-        //Remme
+        //Stor viewboks så der kan være alt i.
+       // Svg svgInnerDrawing = new Svg(900,800,"0,0,900,800",0,0);
+
+        //Carport
+        svg.addRect(0,0,intHeight,intWidth);
+        //svg.addRect(0,0,600,780);
+
+        //Posts
+        svg.addPosts(svg, fullCarport, intWidth, intHeight);
+
+
+
+
+     /*   //Remme
         svg.addRect(0,35,4,780);
         svg.addRect(0,565,4,780);
         svg.addRect(0,565,4,780);
@@ -31,7 +51,7 @@ public class Drawing extends Command {
         svg.addRect(0,565,4,780);
 
         //Spær
-        svg.addRect(0,565,4,780);
+        svg.addRect(0,565,4,780);*/
 
         request.setAttribute("svgdrawing", svg.toString());
         return "design";
