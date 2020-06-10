f3391a1093448d503955d6532b6065d202516a1c
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-06-10 22:14:50 +0200

    Revert "MAJOR BUG FIX"
    This reverts commit 60e21bf38c5220cfb898032fdd28d8db4ad35b27.

--- src/main/java/PresentationLayer/CalculatePrice.java
+++ src/main/java/PresentationLayer/CalculatePrice.java
@@ -11,98 +11,94 @@ import javax.servlet.http.HttpSession;
  * METHOD USED TO CALCULATE THE PRICE OF A CARPORT 
  */
 public class CalculatePrice extends Command {
+
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
-        try {
-            new CreateCarport().execute(request, response);
-            HttpSession session = request.getSession();
+        new CreateCarport().execute(request,response);
+        HttpSession session = request.getSession();
 
-            Product screw = null;
-            Product post = null;
-            Product strap = null;
-            Product fascia = null;
-            Product bracket = null;
-            Product wood = null;
-            Product doorKnob = null;
-            Product doorHinges = null;
-            Product rafter = null;
+        Product screw = null;
+        Product post = null;
+        Product strap = null;
+        Product fascia = null;
+        Product bracket = null;
+        Product wood = null;
+        Product doorKnob = null;
+        Product doorHinges = null;
+        Product rafter = null;
 
-            for (Product product : LogicFacade.getProductList()) {
-                if (product.getName().equals("skrue")) {
-                    screw = product;
-                }
-                if (product.getName().equals("stolpe")) {
-                    post = product;
-                }
-                if (product.getName().equals("rem")) {
-                    strap = product;
-                }
-                if (product.getName().equals("stern")) {
-                    fascia = product;
-                }
-                if (product.getName().equals("beslag")) {
-                    bracket = product;
-                }
-                if (product.getName().equals("beklædning")) {
-                    wood = product;
-                }
-                if (product.getName().equals("dørhåndtag")) {
-                    doorKnob = product;
-                }
-                if (product.getName().equals("dørhængsel")) {
-                    doorHinges = product;
-                }
-                if (product.getName().equals("spær")) {
-                    rafter = product;
-                }
-
+        for (Product product : LogicFacade.getProductList()) {
+            if(product.getName().equals("skrue")) {
+                screw = product;
             }
-            double screwPrice = screw.getPrice();
-            double postPrice = post.getPrice();
-            double strapPrice = strap.getPrice();
-            double fasciaPrice = fascia.getPrice();
-            double bracketPrice = bracket.getPrice();
-            double doorKnobPrice = doorKnob.getPrice();
-            double doorHingesPrice = doorHinges.getPrice();
-            double rafterPrice = rafter.getPrice();
+            if(product.getName().equals("stolpe")){
+                post = product;
+            }
+            if(product.getName().equals("rem")){
+                strap = product;
+            }
+            if(product.getName().equals("stern")){
+                fascia = product;
+            }
+            if(product.getName().equals("beslag")){
+                bracket = product;
+            }
+            if(product.getName().equals("beklædning")){
+                wood = product;
+            }
+            if(product.getName().equals("dørhåndtag")){
+                doorKnob = product;
+            }
+            if(product.getName().equals("dørhængsel")){
+                doorHinges = product;
+            }
+            if(product.getName().equals("spær")){
+                rafter = product;
+            }
 
-            double length = Double.parseDouble(request.getParameter("length"));
-            session.setAttribute("length", length);
-            double width = Double.parseDouble(request.getParameter("width"));
-            session.setAttribute("width", width);
-            boolean wantAShed = Boolean.parseBoolean(request.getParameter("shedYesOrNo"));
-            session.setAttribute("wantAShed", wantAShed);
-            boolean isHalf = Boolean.parseBoolean(request.getParameter("shedLength"));
-            session.setAttribute("shedLength", isHalf);
-
-            boolean isHighRoof = Boolean.parseBoolean(request.getParameter("isHighRoof"));
-            session.setAttribute("isHighRoof", isHighRoof);
-
-            //CARPORT
-            CarportParts carportParts = (CarportParts) session.getAttribute("carportParts");
-            CarportMaterials carportMaterial = carportParts.getCarportMaterials();
-            double carportPrice = new CalculateCarportPartsPrice(carportParts).calculateCarportPartPrice(screwPrice, rafterPrice, postPrice, strapPrice, carportMaterial);
-
-            //Skur
-            Shed shed = (Shed) session.getAttribute("shed");
-            double shedPrice = new CalculateShedPrice(shed).calcShedPrice(doorKnobPrice, doorHingesPrice);
-
-            //TAG
-            Roof roof = (Roof) session.getAttribute("roof");
-            double roofPrice = new CalculateRoofPrice(roof).calcRoofPrice(screwPrice, fasciaPrice, rafterPrice, bracketPrice);
-
-            //Total pris
-            double totalPrice = carportPrice + shedPrice + roofPrice;
-            //Bruges på createOrder
-            session.setAttribute("price", totalPrice);
-            String price = (String.format("%,.0f ,-", totalPrice));
-
-            session.setAttribute("totalPrice", price);
-
-            return "design";
-        } catch (NullPointerException | NumberFormatException e) {
-            request.setAttribute("message", "Hov, husk at udfylde alle felter");
-            return "design";
         }
+        double screwPrice = screw.getPrice();
+        double postPrice = post.getPrice();
+        double strapPrice = strap.getPrice();
+        double fasciaPrice = fascia.getPrice();
+        double bracketPrice = bracket.getPrice();
+        double doorKnobPrice = doorKnob.getPrice();
+        double doorHingesPrice = doorHinges.getPrice();
+        double rafterPrice = rafter.getPrice();
+
+        double length = Double.parseDouble(request.getParameter("length"));
+        session.setAttribute("length", length);
+        double width = Double.parseDouble(request.getParameter("width"));
+        session.setAttribute("width", width);
+        boolean wantAShed = Boolean.parseBoolean(request.getParameter("shedYesOrNo"));
+        session.setAttribute("wantAShed", wantAShed);
+        boolean isHalf = Boolean.parseBoolean(request.getParameter("shedLength"));
+        session.setAttribute("shedLength",isHalf);
+
+        boolean isHighRoof = Boolean.parseBoolean(request.getParameter("isHighRoof"));
+        session.setAttribute("isHighRoof",isHighRoof);
+
+        //CARPORT
+        CarportParts carportParts = (CarportParts) session.getAttribute("carportParts");
+        CarportMaterials carportMaterial = carportParts.getCarportMaterials();
+        double carportPrice = new CalculateCarportPartsPrice(carportParts).calculateCarportPartPrice(screwPrice, rafterPrice, postPrice,strapPrice,carportMaterial);
+
+        //Skur
+        Shed shed = (Shed) session.getAttribute("shed");
+        double shedPrice = new CalculateShedPrice(shed).calcShedPrice(doorKnobPrice, doorHingesPrice);
+
+        //TAG
+        Roof roof = (Roof) session.getAttribute("roof");
+        double roofPrice = new CalculateRoofPrice(roof).calcRoofPrice(screwPrice,fasciaPrice,rafterPrice,bracketPrice);
+
+        //Total pris
+        double totalPrice = carportPrice + shedPrice + roofPrice;
+        //Bruges på createOrder
+        session.setAttribute("price", totalPrice);
+        String price = (String.format("%,.0f ,-", totalPrice));
+
+        session.setAttribute("totalPrice", price);
+
+        return "design";
     }
 }
--- src/main/java/PresentationLayer/Drawing.java
+++ src/main/java/PresentationLayer/Drawing.java
@@ -16,71 +16,66 @@ import javax.servlet.http.HttpSession;
 public class Drawing extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
-        try {
-            HttpSession session = request.getSession();
-            new CalculatePrice().execute(request, response);
-            FullCarport fullCarport = (FullCarport) session.getAttribute("fullCarport");
+        HttpSession session = request.getSession();
+        new CalculatePrice().execute(request, response);
+        FullCarport fullCarport = (FullCarport) session.getAttribute("fullCarport");
 
-            double carportLength = fullCarport.getCarportParts().getLength();
-            double carportWidth = fullCarport.getCarportParts().getWidth();
-            //Converting to metric measurements as they are metric on the actual site.
-            int carportHeigthDB = (int) (carportLength * 100);
-            int carportWidthDB = (int) (carportWidth * 100);
+        double carportLength = fullCarport.getCarportParts().getLength();
+        double carportWidth = fullCarport.getCarportParts().getWidth();
+        //Converting to metric measurements as they are metric on the actual site.
+        int carportHeigthDB = (int) (carportLength * 100);
+        int carportWidthDB = (int) (carportWidth * 100);
 
-            //Aspect Ratio
-            int intCarportLength;
-            int intCarportWidth;
+        //Aspect Ratio
+        int intCarportLength;
+        int intCarportWidth;
 
-            if (carportLength < 5) {
-                intCarportLength = (int) (carportLength * 120);
-                intCarportWidth = (int) (carportWidth * 120);
-            } else {
-                intCarportLength = (int) (carportLength * 100);
-                intCarportWidth = (int) (carportWidth * 100);
-            }
-
-            //Viewbox
-            Svg svg = new Svg(780, 750, "0,0,900,700", 0, 0);
-            //Carport
-            int startX = 0;
-            int startY = 20;
-            svg.addRect(startX, startY, intCarportLength, intCarportWidth);
-
-            // Outer strap
-            svg.addRect(startX + 10, startY + 10, intCarportLength - 20, intCarportWidth - 20);
-
-
-            //Inner straps
-            svg.addStraps(intCarportWidth, intCarportLength);
-
-            //Rafters
-            svg.addRafters(fullCarport, intCarportWidth, intCarportLength);
-
-            //Shed
-            svg.addShedPosts(fullCarport, intCarportWidth);
-
-            //Carport posts
-            svg.addPosts(fullCarport, intCarportWidth, intCarportLength);
-
-            //Pointer vertical
-            int pointerX = intCarportWidth + 20;
-            int pointerY = startY;
-            int finishY = intCarportLength;
-            svg.addVerticalPointer(pointerX, pointerY, finishY, carportHeigthDB);
-
-            //Horizontal pointer
-            pointerX = startX;
-            pointerY = intCarportLength + 20;
-            int finishX = intCarportWidth;
-            svg.addHorizontalPointer(pointerX, pointerY, finishX, carportWidthDB);
-
-            request.setAttribute("svgdrawing", svg.toString());
-
-            request.setAttribute("message", "DIN PLANTEGNING ER NU KLAR");
-        }catch(NullPointerException | NumberFormatException e){
-            request.setAttribute("message", "Hov, husk at udfylde alle felter");
-            return "design";
+        if(carportLength < 5){
+            intCarportLength = (int) (carportLength*120);
+            intCarportWidth = (int) (carportWidth*120);
+        }else{
+            intCarportLength = (int) (carportLength*100);
+            intCarportWidth = (int) (carportWidth*100);
         }
+
+        //Viewbox
+        Svg svg = new Svg(780, 750, "0,0,900,700", 0, 0);
+        //Carport
+        int startX = 0;
+        int startY = 20;
+        svg.addRect(startX, startY, intCarportLength, intCarportWidth);
+
+        // Outer strap
+        svg.addRect(startX+10, startY+10, intCarportLength-20, intCarportWidth-20);
+
+
+        //Inner straps
+        svg.addStraps(intCarportWidth,intCarportLength);
+
+        //Rafters
+        svg.addRafters(fullCarport, intCarportWidth,intCarportLength);
+
+        //Shed
+        svg.addShedPosts(fullCarport, intCarportWidth);
+
+        //Carport posts
+        svg.addPosts(fullCarport, intCarportWidth, intCarportLength);
+
+        //Pointer vertical
+        int pointerX = intCarportWidth + 20;
+        int pointerY = startY;
+        int finishY = intCarportLength;
+        svg.addVerticalPointer(pointerX, pointerY,finishY, carportHeigthDB);
+
+        //Horizontal pointer
+        pointerX = startX;
+        pointerY = intCarportLength + 20;
+        int finishX = intCarportWidth;
+        svg.addHorizontalPointer(pointerX, pointerY, finishX,carportWidthDB);
+
+        request.setAttribute("svgdrawing", svg.toString());
+
+        request.setAttribute("message","DIN PLANTEGNING ER NU KLAR");
         return "design";
     }
 }
